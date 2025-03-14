6d56ebd838ada7d8544797bd71f7f35f59ece58e
Timmy-Tee <timmyaof02@gmail.com>
2025-03-14 12:20:03 +0100

    Updated Hompage

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ location, text, nav }) => {
   return (
     <Link
       to={`${location ? location : "/"}`}
-      className={`${nav ? "text-main border hover:border-main  ease-in-out py-[6px] w-fit  ml-4 px-5 text-center rounded-[5px] hover:bg-main hover:text-white" : "text-white bg-main px-[30px] py-[10px] text-[15px] rounded-[9px]"}`}
+      className={`${nav ? "text-main border hover:border-main  ease-in-out py-[6px] w-fit  ml-4 px-5 text-center rounded-[5px] hover:bg-main hover:text-white" : "text-white bg-main px-[30px] py-[10px] text-[15px] rounded-[9px] font-inter"}`}
     >
       <p> {text} </p>
     </Link>
