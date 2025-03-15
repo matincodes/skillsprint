b6a8c33ab1525d3dddeb2b7c4514ae8acc0f22b9
Abdulmatin Adeniji <abdulmatinadeniji@gmail.com>
2025-03-15 10:18:18 +0100

    Refactor axios instance to enable request and response interceptors for token management

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: "https://skillsprint.up.railway.app",
@@ -10,44 +10,44 @@ const instance = axios.create({
 });
 
 // Request interceptor to add access token to headers
-// instance.interceptors.request.use(
-//   (config) => {
-//     const accessToken = Cookies.get("accessToken");
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error),
-// );
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
-// // Response interceptor to handle token expiration
-// instance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (error.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-//       try {
-//         const refreshToken = Cookies.get("refreshToken");
-//         const response = await axios.post(
-//           "/api/auth/refresh-token",
-//           { refreshToken },
-//           { withCredentials: true },
-//         );
-//         if (response.status === 200) {
-//           Cookies.set("accessToken", response.data.accessToken, {
-//             httpOnly: true,
-//           });
-//           originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
-//           return axios(originalRequest);
-//         }
-//       } catch (err) {
-//         console.error("Token refresh error:", err);
-//       }
-//     }
-//     return Promise.reject(error);
-//   },
-// );
+// Response interceptor to handle token expiration
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = Cookies.get("refreshToken");
+        const response = await axios.post(
+          "https://skillsprint.up.railway.app/api/auth/refresh-token",
+          { refreshToken },
+          { withCredentials: true },
+        );
+        if (response.status === 200) {
+          Cookies.set("accessToken", response.data.accessToken, {
+            httpOnly: true,
+          });
+          originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+          return axios(originalRequest);
+        }
+      } catch (err) {
+        console.error("Token refresh error:", err);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
