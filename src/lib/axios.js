da38a53bc9c09b25fbfca631dd92f3a257522888
Abdulmatin Adeniji <abdulmatinadeniji@gmail.com>
2025-03-16 15:08:27 +0100

    Add @tanstack/react-query for data fetching and implement QueryClientProvider

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -11,29 +11,31 @@ const instance = axios.create({
 });
 
 // Response interceptor for handling token refresh
-// instance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-//     // If error is 401 and we haven't tried to refresh the token yet
-//     if (error.response?.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
+    // If error is 401 and we haven't tried to refresh the token yet
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
 
-//       try {
-//         // Call the refresh token endpoint
-//         await axios.get(`${baseURL}/api/auth/token`, { withCredentials: true });
+      try {
+        // Call the refresh token endpoint
+        await axios.get("https://skillsprint.up.railway.app/api/auth/token", {
+          withCredentials: true,
+        });
 
-//         // Retry the original request
-//         return instance(originalRequest);
-//       } catch (refreshError) {
-//         // If refresh token fails, redirect to login
-//         window.location.href = "/student/login";
-//         return Promise.reject(refreshError);
-//       }
-//     }
+        // Retry the original request
+        return instance(originalRequest);
+      } catch (refreshError) {
+        // If refresh token fails, redirect to login
+        window.location.href = "/student/login";
+        return Promise.reject(refreshError);
+      }
+    }
 
-//     return Promise.reject(error);
-//   },
-// );
+    return Promise.reject(error);
+  },
+);
 export default instance;
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import { EnrollmentProvider, useEnrollment } from "./context/EnrollmentContext";
+
+
+const queryClient = new QueryClient();
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -18,12 +22,14 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
+      <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <EnrollmentProvider>
           <App />
         </EnrollmentProvider>
         <ToastContainer />
       </AuthProvider>
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
