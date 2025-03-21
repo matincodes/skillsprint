b43c8a0a730d344ab8e0c1f771b0702fa29bdc91
Divine <divinetidy@gmail.com>
2025-03-21 23:23:37 +0100

    assignment section

---
+++ src/data/assignment.jsx
@@ -0,0 +1,30 @@
+const assignmentsData = [
+    {
+      title: "Week 1 - Understanding Auto-layout",
+      description:
+        "Learn the essentials of UI/UX design, including usability, wireframing, and user-centered design principles. Learn the essentials of UI/UX design, including usability, wireframing, and user-centered design principles. ",
+      dueDate: "Due : Feb 2, 11 : 59 PM",
+      status: "Pending",
+      category:"Past Assignments"
+    },
+    {
+      title: "Week 1 - Understanding Auto-layout",
+      description:
+        "Learn the essentials of UI/UX design, including usability, wireframing, and user-centered design principles. Learn the essentials of UI/UX design, including usability, wireframing, and user-centered design principles. ",
+      dueDate: "Due : may 10, 11 : 59 PM",
+      status: "Ongoing",
+      category:"Pending Assignments"
+
+    },
+    {
+      title: "Week 1 - Understanding Auto-layout",
+      description:
+        "Learn the essentials of UI/UX design, including usability, wireframing, and user-centered design principles. Learn the essentials of UI/UX design, including usability, wireframing, and user-centered design principles. ",
+      dueDate: "Due : Feb 10, 11 : 59 PM",
+      status: "Completed",
+      category:"Past Assignments"
+
+    },
+  ];
+
+  export default assignmentsData;
